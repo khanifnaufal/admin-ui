2facf254e1ab0331d252d89b0866a8725480ee58
Khanifnaufal <mkhanif86@gmail.com>
2025-10-14 11:35:41 +0700

    membuat page sign in

--- src/App.jsx
+++ src/App.jsx
@@ -2,127 +2,130 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="p-6 space-y-8">
-        {/* LATIHAN 1 - NAVBAR */}
-        LATIHAN 1
-        <nav className="flex flex-col md:flex-row justify-between bg-gray-100 p-4">
-          {/* Kiri - Logo */}
-          <div className="font-bold text-lg mb-2 md:mb-0">MyLogo</div>
-
-          {/* Kanan - Menu */}
-          <ul className="flex flex-col md:flex-row gap-2 md:gap-6 text-sm">
-            <li>
-              <a href="#" className="hover:underline">  
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <hr />
-        {/* LATIHAN 2 - GRID GALERI */}
-        LATIHAN 2
-          <div className="p-4 grid grid-cols-2 md:grid-cols-4 gap-4">
-            <img
-              src="https://www.placehold.co/200x200"
-              alt="Gambar Galeri"
-              className="w-full h-auto"
-            />
-            <img
-              src="https://www.placehold.co/200x200"
-              alt="Gambar Galeri"
-              className="w-full h-auto"
-            />
-            <img
-              src="https://www.placehold.co/200x200"
-              alt="Gambar Galeri"
-              className="w-full h-auto"
-            />
-            <img
-              src="https://www.placehold.co/200x200"
-              alt="Gambar Galeri"
-              className="w-full h-auto"
-            />
-            <img
-              src="https://www.placehold.co/200x200"
-              alt="Gambar Galeri"
-              className="w-full h-auto"
-            />
-            <img
-              src="https://www.placehold.co/200x200"
-              alt="Gambar Galeri"
-              className="w-full h-auto"
-            />
+    <main className="flex justify-center items-center bg-special-mainBg min-h-screen">
+      {/* container start */}
+      <div className=" w-full max-w-sm">
+        {/* logo start */}
+        <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
+          <span className="font-bold">FINE</span>
+          bank
+          <span className="font-bold">.IO</span>
+        </div> 
+        {/* logo end */}
+        {/* form start */}
+        <div></div>
+        {/* form end */}
+        {/* teks start */}
+        <div className="mt-16">
+          <form action="">
+            <div className="mb-6">
+              <label htmlFor="email" className="block text-sm mb-2">
+                Email Address
+              </label>
+              <input
+                type="email"
+                className=" py-3 pl-4 text-sm rounded-md w-full bg-special-mainBg border border-gray-03 text-gray-01 focus:border-black focus:outline-none focus:ring-0"
+                placeholder="taufikgimang@gmail.com"
+                name="email"
+                id="email"/>
+            </div>
+            <div className="mb-6">
+              <div className="flex justify-between items-center mb-2 text-sm">
+                <label htmlFor="password" className="block">
+                  Password
+                </label>
+                <a href="#" className="text-primary hover:underline">
+                  Forgot password?
+                </a>
+              </div>
+              <input 
+                type="password"
+                className="py-3 pl-4 text-sm rounded-md w-full bg-special-mainBg border border-gray-03 text-gray-01 focus:border-black focus:outline-none focus:ring-0"
+                placeholder="*********"
+                name="password"
+                id="password"
+              />
+            </div>
+            <div className="mb-3 flex items-center">
+              <input
+              type="checkbox"
+              className="text-sm accent-primary" 
+              name="status"
+              id="status"
+              />
+              <label htmlFor="status" className="text-sm text-gray-01 ml-3">
+                Keep me signed in
+              </label>
+            </div>
+            <button className=" h-12 rounded-md text-sm bg-primary w-full text-white">
+              Login
+            </button>
+          </form>
         </div>
-        <hr className="my-4 "/>
-        {/* LATIHAN 3 - PRICING TABLE */}
-        LATIHAN 3
-        <div className="p-4 grid grid-cols-1 md:grid-cols-3 gap-4 text-center">
-        <div className="bg-gray-200 p-6 rounded shadow-sm">
-          <div className="font-medium">Basic</div>
-          <div>Rp 50.000</div>
+        <div className="my-9 px-7 flex flex-col justify-center items-center text-xs text-gray-03">
+          <div className="border border-gray-05 w-full"></div>
+          <div class="bg-special-mainBg absolute px-2"> or Sign in With</div>
         </div>
-
-        <div className="bg-blue-100 p-6 rounded shadow-md">
-          <div className="font-medium">Pro</div>
-          <div>Rp 100.000</div>
+        {/* teks end */}
+        {/* sign in with google start */}
+        <div>
+          <button className=" h-12 flex items-center justify-center rounded-sm text-sm w-full bg-gray-05 text-gray-1">
+          <svg
+              class="h-6 w-6 mr-2"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              width="800px"
+              height="800px"
+              viewBox="-0.5 0 48 48"
+              version="1.1"
+            >
+              <title>Google-color</title> <desc>Created with Sketch.</desc>
+              <defs> </defs>
+              <g
+                id="Icons"
+                stroke="none"
+                stroke-width="1"
+                fill="none"
+                fill-rule="evenodd"
+              >
+                <g id="Color-" transform="translate(-401.000000, -860.000000)">
+                  <g id="Google" transform="translate(401.000000, 860.000000)">
+                    <path
+                      d="M9.82727273,24 C9.82727273,22.4757333 10.0804318,21.0144 10.5322727,19.6437333 L2.62345455,13.6042667 C1.08206818,16.7338667 0.213636364,20.2602667 0.213636364,24 C0.213636364,27.7365333 1.081,31.2608 2.62025,34.3882667 L10.5247955,28.3370667 C10.0772273,26.9728 9.82727273,25.5168 9.82727273,24"
+                      id="Fill-1"
+                      fill="#FBBC05"
+                    ></path>
+                    <path
+                      d="M23.7136364,10.1333333 C27.025,10.1333333 30.0159091,11.3066667 32.3659091,13.2266667 L39.2022727,6.4 C35.0363636,2.77333333 29.6954545,0.533333333 23.7136364,0.533333333 C14.4268636,0.533333333 6.44540909,5.84426667 2.62345455,13.6042667 L10.5322727,19.6437333 C12.3545909,14.112 17.5491591,10.1333333 23.7136364,10.1333333"
+                      id="Fill-2"
+                      fill="#EB4335"
+                    ></path>
+                    <path
+                      d="M23.7136364,37.8666667 C17.5491591,37.8666667 12.3545909,33.888 10.5322727,28.3562667 L2.62345455,34.3946667 C6.44540909,42.1557333 14.4268636,47.4666667 23.7136364,47.4666667 C29.4455,47.4666667 34.9177955,45.4314667 39.0249545,41.6181333 L31.5177727,35.8144 C29.3995682,37.1488 26.7323182,37.8666667 23.7136364,37.8666667"
+                      id="Fill-3"
+                      fill="#34A853"
+                    ></path>
+                    <path
+                      d="M46.1454545,24 C46.1454545,22.6133333 45.9318182,21.12 45.6113636,19.7333333 L23.7136364,19.7333333 L23.7136364,28.8 L36.3181818,28.8 C35.6879545,31.8912 33.9724545,34.2677333 31.5177727,35.8144 L39.0249545,41.6181333 C43.3393409,37.6138667 46.1454545,31.6490667 46.1454545,24"
+                      id="Fill-4"
+                      fill="#4285F4"
+                    ></path>
+                  </g>
+                </g>
+              </g>
+            </svg>
+            <span>Continue with Google</span>
+          </button>
         </div>
-
-        <div className="bg-gray-200 p-6 rounded shadow-sm">
-          <div className="font-medium">Premium</div>
-          <div>Rp 200.000</div>
+        {/* sign in with google end */}
+        {/* link start */}
+        <div className="flex justify-center mt-6">
+          <a className="text-primary text-sm font-bold">Create an account</a>
         </div>
+        {/* link end */}
       </div>
-        <hr />
-        {/* LATIHAN 4 - DASHBOARD LAYOUT */}
-        LATIHAN 4
-        <div className="grid grid-cols-1 md:grid-cols-4 min-h-screen gap-1">
-          {/* Header */}
-          <header className="bg-gray-300 p-4 md:col-span-4 text-center md:text-left">
-            Header
-          </header>
-
-          {/* Sidebar */}
-          <aside className="bg-gray-200 p-4 md:col-span-1">
-            Sidebar
-          </aside>
-
-          {/* Content */}
-          <main className="bg-white p-4 border md:col-span-3">
-            Content
-          </main>
-
-          {/* Footer */}
-          <footer className="bg-gray-300 p-4 md:col-span-4 text-center">
-            Footer
-          </footer>
-        </div>
-        <hr />
-        {/* LATIHAN 5 - CARD PRODUCT */}
-        LATIHAN 5
-        <div className="p-4 border flex flex-col items-center gap-2 sm:flex-row sm:items-center">
-          <img
-            src="https://www.placehold.co/80x80"
-            alt="Gambar Produk"
-            className="h-20 w-20"
-          />
-          <div>
-            <h3 className="font-bold">Nama Produk</h3>
-            <p className="text-sm text-gray-600">Deskripsi produk singkat...</p>
-          </div>
-        </div>
-      </div>
-    </>
+      {/* container end */}
+    </main>
   );
 }
 
