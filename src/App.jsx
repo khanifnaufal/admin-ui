ba68fa29d3e6b5192a00ee31f1d9da05be565d59
khanifnaufal <mkhanif86@gmail.com>
2025-12-09 11:48:34 +0700

    responsive layout

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import SignInPage from "./pages/signIn";
+import SignUpPage from "./pages/signUp";
+import ErrorPage from "./pages/error";
+import DashboardPage from "./pages/dashboard";
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
+
+function App() {
+  const myRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <DashboardPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <SignInPage />,
+    },
+    {
+      path: "/register",
+      element: <SignUpPage />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={myRouter} />
+    </>
+  );
+}
+
+export default App;
