226a4310da4b7993311dd3442d53c8641aa52d07
khanifnaufal <mkhanif86@gmail.com>
2026-01-04 20:38:42 +0700

    frontend-backend integration

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import MainLayout from '../components/Layouts/MainLayout';
 import Card from '../components/Elements/Card';
 import CardBalance from '../components/Fragments/CardBalance';
@@ -7,36 +7,56 @@ import CardUpcomingBill from '../components/Fragments/CardUpcomingBill';
 import CardRecentTransaction from '../components/Fragments/CardRecentTransaction';
 import CardStatistic from '../components/Fragments/CardStatistic';
 import CardExpsensesBreakdown from '../components/Fragments/CardExpenseBreakdown';
-import { transactions,bills,expensesBreakdowns,balances,goals,expensesStatistics } from '../data';
+import { transactions, bills, expensesBreakdowns, balances, goals, expensesStatistics } from '../data';
+import { goalService } from '../services/dataService';
+import { AuthContext } from '../context/authContext';
 
 function dashboard() {
-    console.log(transactions);
-  return (
-    <>
-        <MainLayout>
-            <div className="grid sm:grid-cols-12  gap-6">
-                <div className='sm:col-span-4'>
-                    <CardBalance data={balances} />
+    const [goals, setGoals] = useState({});
+
+    const fetchGoals = async () => {
+        try {
+            const data = await goalService();
+            setGoals(data);
+        } catch (err) {
+            console.error("Gagal mengambil data goals:", err);
+            if (err.status === 401) {
+                logout();
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchGoals();
+    }, []);
+
+    console.log(goals);
+    return (
+        <>
+            <MainLayout>
+                <div className="grid sm:grid-cols-12  gap-6">
+                    <div className='sm:col-span-4'>
+                        <CardBalance data={balances} />
+                    </div>
+                    <div className='sm:col-span-4'>
+                        <CardGoal data={goals} />
+                    </div>
+                    <div className='sm:col-span-4'>
+                        <CardUpcomingBill data={bills} />
+                    </div>
+                    <div className="sm:col-span-4 sm:row-span-2">
+                        <CardRecentTransaction data={transactions} />
+                    </div>
+                    <div className="sm:col-span-8">
+                        <CardStatistic data={expensesStatistics} />
+                    </div>
+                    <div className="sm:col-span-8">
+                        <CardExpsensesBreakdown data={expensesBreakdowns} />
+                    </div>
                 </div>
-                <div className='sm:col-span-4'>
-                    <CardGoal data={goals}/>
-                </div>
-                <div className='sm:col-span-4'>
-                    <CardUpcomingBill data={bills}/>
-                </div>
-                <div className="sm:col-span-4 sm:row-span-2">
-                    <CardRecentTransaction data={transactions}/>
-                </div>
-                <div className="sm:col-span-8">
-                    <CardStatistic data={expensesStatistics}/>
-                </div>
-                <div className="sm:col-span-8">
-                    <CardExpsensesBreakdown data={expensesBreakdowns}/>
-                </div>
-            </div>
-        </MainLayout>
-    </>
-  )
+            </MainLayout>
+        </>
+    )
 }
 
 export default dashboard
